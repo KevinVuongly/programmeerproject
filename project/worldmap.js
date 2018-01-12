94828a2c1d929d3aafff02b06ec0de9ab3058c74
KevinVuongly <k.vuongly@gmail.com>
2018-01-12 12:49:30 +0100

    edit repository

--- project/worldmap.js
+++ project/worldmap.js
@@ -1,19 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+    var format = d3.format(",");
+
     // Set tooltips
     var tip = d3.tip()
                 .attr('class', 'd3-tip')
                 .offset([-10, 0])
                 .html(function(d) {
-                  return "<strong>Country: </strong><span class='details'>" + d.properties.name + "<br></span>" + "<strong>Population: </strong><span class='details'>" + d.population +"</span>";
+                  return "<strong>Country: </strong><span class='details'>" + d.properties.name + "<br></span>" + "<strong>PISA score: </strong><span class='details'>" + format(d.Accumulated) +"</span>";
                 })
 
     var margin = {top: 20, right: 20, bottom: 30, left: 30},
-                width = 500 - margin.left - margin.right,
+                width = 600 - margin.left - margin.right,
                 height = 500 - margin.top - margin.bottom;
 
     var color = d3.scale.threshold()
-        .domain([10000,100000,500000,1000000,5000000,10000000,50000000,100000000,500000000,1500000000])
+        .domain([1150,1200,1290,1380,1450,1500,1520,1560,1580,1650])
         .range(["rgb(135,0,0)", "rgb(196,0,0)", "rgb(255,34,0)",
                 "rgb(250,138,10)", "rgb(250,226,10)", "rgb(202,250,10)",
                 "rgb(115,242,18)","rgb(4,179,16)","rgb(7,129,17)","rgb(9,80,5)"]);
@@ -37,14 +39,14 @@ document.addEventListener("DOMContentLoaded", function() {
 
     queue()
         .defer(d3.json, "world_countries.json")
-        .defer(d3.tsv, "world_population.tsv")
+        .defer(d3.csv, "data/PISA2012.csv")
         .await(ready);
 
-    function ready(error, data, population) {
-      var populationById = {};
+    function ready(error, data, pisa) {
+      var pisaById = {};
 
-      population.forEach(function(d) { populationById[d.id] = +d.population; });
-
+      pisa.forEach(function(d) { pisaById[d.id] = +d.Accumulated; });
+      data.features.forEach(function(d) { d.Accumulated = pisaById[d.id] });
 
       svg.append("g")
           .attr("class", "countries")
@@ -52,7 +54,7 @@ document.addEventListener("DOMContentLoaded", function() {
           .data(data.features)
         .enter().append("path")
           .attr("d", path)
-          .style("fill", function(d) { return color(populationById[d.id]); })
+          .style("fill", function(d) { return color(pisaById[d.id]); })
           .style('stroke', 'white')
           .style('stroke-width', 1.5)
           .style("opacity",0.8)
